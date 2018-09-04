f3085bbbda2d44082c2c711ec2009624ac83bcf2
huangtao <273470570@qq.com>
2018-09-04 09:32:24 +0800

    更新Database.java mysql和db.changelog.h2mysql.xml

--- ngrinder-controller/src/main/java/org/ngrinder/infra/config/Database.java
+++ ngrinder-controller/src/main/java/org/ngrinder/infra/config/Database.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package org.ngrinder.infra.config;
 
@@ -22,6 +22,7 @@ import org.hibernate.dialect.H2ExDialect;
 import org.ngrinder.common.util.PropertiesWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 import java.sql.Driver;
 
@@ -68,7 +69,22 @@ public enum Database {
 			dataSource.setUsername(databaseProperties.getProperty(DatabaseConfig.PROP_DATABASE_USERNAME));
 			dataSource.setPassword(databaseProperties.getProperty(DatabaseConfig.PROP_DATABASE_PASSWORD));
 		}
+	},
+
+	/**
+	 * mysql
+	 */
+	mysql(com.mysql.jdbc.Driver.class, org.hibernate.dialect.MySQL5Dialect.class, "jdbc:mysql://%s?%s") {
+		@Override
+		protected void setupVariants(BasicDataSource dataSource, PropertiesWrapper databaseProperties) {
+			dataSource.setUrl(String.format(getUrlTemplate(),
+				databaseProperties.getProperty(DatabaseConfig.PROP_DATABASE_URL),
+				StringUtils.trimToEmpty(databaseProperties.getProperty(DatabaseConfig.PROP_DATABASE_URL_OPTION))));
+			dataSource.setUsername(databaseProperties.getProperty(DatabaseConfig.PROP_DATABASE_USERNAME));
+			dataSource.setPassword(databaseProperties.getProperty(DatabaseConfig.PROP_DATABASE_PASSWORD));
+		}
 	};
+
 
 	/*
 	 * Default db constants
