208cabb1e7e7c05fd1d95f2542866a07e158dbb4
huangtao <273470570@qq.com>
2018-09-24 12:35:28 +0800

    更新进程线程数策略，默认1进程

--- ngrinder-controller/src/main/resources/ngrinder_home_template/process_and_thread_policy.js
+++ ngrinder-controller/src/main/resources/ngrinder_home_template/process_and_thread_policy.js
@@ -1,18 +1,20 @@
 function getProcessCount(total) {
-	if (total < 2) {
-		return 1;
-	}
-	
-	var processCount = 2;
-
-	if (total > 80) {
-		processCount = parseInt(total / 40) + 1;
-	}
-	
-	if (processCount > 10) {
-		processCount = 10;
-	}
-	return processCount;
+	// if (total < 2) {
+	// 	return 1;
+	// }
+	//
+	// var processCount = 2;
+    //
+	// if (total > 80) {
+	// 	processCount = parseInt(total / 40) + 1;
+	// }
+	//
+	// if (processCount > 10) {
+	// 	processCount = 10;
+	// }
+	// return processCount;
+	// 修改进程/线程数分配策略，默认进程数为1
+    return 1;
 }
 
 function getThreadCount(total) {
